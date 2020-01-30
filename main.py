96f7bf1ac8482abcf6f0080814a49a3cb54150a6
Alexander <alexanderkoltygin@gmail.com>
2020-01-30 18:32:17 +0600

    Change code

--- main.py
+++ main.py
@@ -2,25 +2,25 @@ import numpy as np
 
 
 def neural_network(input, weights):
-    pred = vect_mul_sum(input, weights)
+    hid = input.dot(weights[0])
+    pred = hid.dot(weights[1])
+    pred = [round(elem, 3) for elem in pred]
     return pred
 
 
-def vect_mul_sum(vec, matrix):
-    assert(len(vec) == len(matrix))
-    output = [0, 0, 0]
+in_wgt = np.array([
+    [0.1, 0.2, -0.1],
+    [-0.1, 0.1, 0.9],
+    [0.1, 0.4, 0.1]
+]).T
 
-    for i in range(len(vec)):
-        output[i] = round(vec.dot(matrix[i]), 5)
-
-    return output
-
-
-weights = [
-    [0.1, 0.1, -0.3],
+hp_wgt = np.array([
+    [0.3, 1.1, -0.3],
     [0.1, 0.2, 0.0],
     [0.0, 1.3, 0.1]
-]
+]).T
+
+weights = [in_wgt, hp_wgt]
 
 toes = np.array([8.5, 9.5, 9.9, 9.0])
 wlrec = np.array([0.65, 0.8, 0.8, 0.9])
